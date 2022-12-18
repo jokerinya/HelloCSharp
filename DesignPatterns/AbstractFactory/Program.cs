458f2019055561a89ddddf81a7e88a8ae0cbbb66
Jokerinya <sakacibrahim@gmail.com>
2022-12-18 22:25:27 +0200

    factory desing pattern has been studied

---
+++ DesignPatterns/AbstractFactory/Program.cs
@@ -0,0 +1,116 @@
+﻿using System;
+using System.Collections.Concurrent;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AbstractFactory
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            ProductManager productManager = new ProductManager(new Factory1());
+            //ProductManager productManager = new ProductManager(new Factory2());
+            productManager.GetAll();
+            Console.ReadLine();
+
+        }
+    }
+    public abstract class Logging
+    {
+        public abstract void Log(string message);
+    }
+
+    public abstract class Caching
+    {
+        public abstract void Cache(string message);
+    }
+
+    public class Log4NetLogger : Logging
+    {
+        public override void Log(string message)
+        {
+            Console.WriteLine("Logged with log4net");
+        }
+    }
+
+    public class NLogger : Logging
+    {
+        public override void Log(string message)
+        {
+            Console.WriteLine("Logged with nlogger");
+        }
+    }
+
+    public class MemCache : Caching
+    {
+        public override void Cache(string message)
+        {
+            Console.WriteLine("Cached with memcache");
+        }
+    }
+
+    public class RedisCache : Caching
+    {
+        public override void Cache(string message)
+        {
+            Console.WriteLine("Cached with redis cahce");
+        }
+    }
+
+    public abstract class CrossCuttingConcernsFactory
+    {
+        public abstract Logging CreateLogger();
+        public abstract Caching CreateCaching();
+    }
+
+    public class Factory1 : CrossCuttingConcernsFactory
+    {
+        public override Caching CreateCaching()
+        {
+            return new RedisCache();
+        }
+
+        public override Logging CreateLogger()
+        {
+            return new Log4NetLogger();
+        }
+    }
+
+    public class Factory2 : CrossCuttingConcernsFactory
+    {
+        public override Caching CreateCaching()
+        {
+            return new RedisCache();
+        }
+
+        public override Logging CreateLogger()
+        {
+            return new NLogger();
+        }
+    }
+
+    public class ProductManager
+    {
+        private CrossCuttingConcernsFactory _crossCuttingConcernsFactory;
+
+        private Logging _logging;
+        private Caching _caching;
+
+        public ProductManager(CrossCuttingConcernsFactory crossCuttingConcernsFactory)
+        {
+            _crossCuttingConcernsFactory = crossCuttingConcernsFactory;
+            _caching = _crossCuttingConcernsFactory.CreateCaching();
+            _logging = _crossCuttingConcernsFactory.CreateLogger();
+        }
+
+        public void GetAll()
+        {
+            _logging.Log("Logged");
+            _caching.Cache("Cached");
+            Console.WriteLine("Products listed!");
+        }
+    }
+}
---
+++ DesignPatterns/FactoryMethod/Program.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FactoryMethod
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // One of the most used
+            // User is not depended to LoggerFactory or Logger 
+            CustomerManager customerManager = new CustomerManager(new LoggerFactory());
+            customerManager.Save();
+        }
+    }
+    public class LoggerFactory : ILoggerFactory
+    {
+        // Give me a logger according to bussiness requirements 
+        public ILogger CreateLogger()
+        {
+            return new EdLogger();
+        }
+    }
+    public interface ILoggerFactory
+    {
+        ILogger CreateLogger();
+    }
+    public interface ILogger
+    {
+        void Log();
+    }
+    public class EdLogger : ILogger
+    {
+        public void Log()
+        {
+            Console.WriteLine("Logged with EdLogger");
+        }
+
+    }
+    public class CustomerManager
+    {
+        private ILoggerFactory _loggerFactory;
+        public CustomerManager(ILoggerFactory loggerFactory)
+        {
+            _loggerFactory = loggerFactory;
+        }
+        public void Save()
+        {
+            Console.WriteLine("Saved!");
+            //ILogger logger = new LoggerFactory().CreateLogger();
+            ILogger logger = _loggerFactory.CreateLogger();
+            logger.Log();
+        }
+    }
+}
