2fd017df43009fc92051d6d317f633f451a1c0a1
Jokerinya <sakacibrahim@gmail.com>
2022-12-21 17:10:38 +0200

    adapter pattern has been observed

---
+++ DesignPatterns/Adapter/Program.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Adapter
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // Outer service will be added to our projects
+            // How can we deal with dependency and abstraction?
+
+            //ProductManager productManager = new ProductManager(new EdLogger());
+            ProductManager productManager = new ProductManager(new Log4NetAdapter(new Log4Net()));
+            productManager.Save();
+            Console.ReadLine();
+        }
+    }
+
+    class ProductManager
+    {
+        private ILogger _logger;
+
+        public ProductManager(ILogger logger)
+        {
+            _logger = logger;
+        }
+
+        public void Save()
+        {
+            _logger.Log("User data");
+            Console.WriteLine("Saved");
+        }
+    }
+
+    interface ILogger
+    {
+        void Log(string message);
+    }
+
+    class EdLogger : ILogger
+    {
+        public void Log(string message)
+        {
+            Console.WriteLine("Logged, {0}", message);
+        }
+    }
+
+    // Let's say we want to change our Logger from the EdLogger to Log4Net
+    // It came from NuGet and we can't touch Log4Net class (dll)
+    class Log4Net
+    {
+        public void LogMessage(string message)
+        {
+            Console.WriteLine("Logged with log4net, {0}", message);
+        }
+    }
+
+    // I create an adapter for it and call it's logger method inside this.
+    class Log4NetAdapter : ILogger
+    {
+        private Log4Net _logger;
+
+        public Log4NetAdapter(Log4Net logger)
+        {
+            _logger = logger;
+        }
+
+        public void Log(string message)
+        {
+            _logger.LogMessage(message);
+        }
+    }
+}
---
+++ DesignPatterns/Facade/Program.cs
@@ -0,0 +1,106 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Facade
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            CustomerMaganer customerMaganer = new CustomerMaganer();
+            customerMaganer.Save();
+            Console.ReadLine();
+        }
+    }
+
+    class Logging : ILogging
+    {
+        public void Log()
+        {
+            Console.WriteLine("Logged");
+        }
+    }
+
+    internal interface ILogging
+    {
+        void Log();
+    }
+
+    class Cahching : ICahching
+    {
+        public void Cache()
+        {
+            Console.WriteLine("Cached");
+        }
+    }
+
+    internal interface ICahching
+    {
+        void Cache();
+    }
+
+    class Authorize : IAuthorize
+    {
+        public void CheckUser()
+        {
+            Console.WriteLine("User checked");
+        }
+    }
+
+    internal interface IAuthorize
+    {
+        void CheckUser();
+    }
+
+    class Validating : IValidating
+    {
+        public void Validate()
+        {
+            Console.WriteLine("Info validated");
+        }
+    }
+
+    internal interface IValidating
+    {
+        void Validate();
+    }
+
+    class CustomerMaganer
+    {
+        private CrossCuttingConcernsFacade _concerns;
+        public CustomerMaganer()
+        {
+            _concerns = new CrossCuttingConcernsFacade();
+        }
+
+        public void Save()
+        {
+            _concerns.Cahching.Cache();
+            _concerns.Authorize.CheckUser();
+            _concerns.Logging.Log();
+            _concerns.Validating.Validate();
+            Console.WriteLine("Saved");
+        }
+    }
+
+    // Create this and only change here for dependencies
+    class CrossCuttingConcernsFacade
+    {
+        public ILogging Logging;
+        public ICahching Cahching;
+        public IAuthorize Authorize;
+        // Validating added later
+        public IValidating Validating;
+
+        public CrossCuttingConcernsFacade()
+        {
+            Logging = new Logging();
+            Cahching = new Cahching();
+            Authorize = new Authorize();
+            Validating = new Validating();
+        }
+    }
+}
