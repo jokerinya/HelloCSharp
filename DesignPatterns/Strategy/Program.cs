ee71ee2e197373b96f7de426e0b2b727b1e11882
Jokerinya <sakacibrahim@gmail.com>
2023-01-01 16:30:37 +0200

    strategy pattern has been observed

---
+++ DesignPatterns/Strategy/Program.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Strategy
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            CustomerManager customerManager = new CustomerManager();
+            customerManager.CreditCalculatorBase = new Before2010CreditCalculator();
+            customerManager.SaveCredit();
+            Console.ReadLine();
+        }
+    }
+
+    abstract class CreditCalculatorBase
+    {
+        public abstract void Calculator();
+    }
+
+    class Before2010CreditCalculator : CreditCalculatorBase
+    {
+        public override void Calculator()
+        {
+            Console.WriteLine("Credit calculated using before 2010");
+        }
+    }
+    
+    class After2010CreditCalculator : CreditCalculatorBase
+    {
+        public override void Calculator()
+        {
+            Console.WriteLine("Credit calculated using after 2010");
+        }
+    }
+
+    class CustomerManager
+    {
+        public CreditCalculatorBase CreditCalculatorBase { get; set; }
+
+        public void SaveCredit()
+        {
+            CreditCalculatorBase.Calculator();
+            Console.WriteLine("Customer manger business");
+        }
+    }
+}
