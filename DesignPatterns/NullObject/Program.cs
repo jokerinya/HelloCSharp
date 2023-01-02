d4c8959a469eb5198886c81ffa2b8f50da27f21a
Jokerinya <sakacibrahim@gmail.com>
2023-01-02 18:42:01 +0200

    null object pattern has been observed

---
+++ DesignPatterns/NullObject/Program.cs
@@ -0,0 +1,96 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NullObject
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            CustomerManager customerManager = new CustomerManager(new NLogLogger());
+            customerManager.Save();
+
+            Console.ReadLine();
+        }
+    }
+
+    interface ILogger
+    {
+        void Log();
+    }
+
+    class Log4NetLogger : ILogger
+    {
+        public void Log()
+        {
+            Console.WriteLine("Logged with Log 4 Net");
+        }
+    }
+
+    class NLogLogger : ILogger
+    {
+        public void Log()
+        {
+            Console.WriteLine("Logged with NLogLogger.");
+        }
+    }
+
+    class CustomerManager
+    {
+        private ILogger _logger;
+
+        public CustomerManager(ILogger logger)
+        {
+            _logger = logger;
+        }
+
+        public void Save()
+        {
+            Console.WriteLine("Saved(Customer Manager)");
+            _logger.Log();
+        }
+
+    }
+
+    class StubLogger : ILogger
+    {
+        // Let's make this fake logger as singleton
+        private static StubLogger _stubLogger;
+        private static object _lock = new object();
+
+        private StubLogger() { }
+
+        public static StubLogger GetLogger()
+        {
+            // lock is useful for multi threading
+            lock (_lock)
+            {
+                if (_stubLogger == null)
+                {
+                    _stubLogger = new StubLogger();
+                }
+            }
+            return _stubLogger;
+        }
+
+        public void Log()
+        {
+
+        }
+    }
+
+    // Test code
+
+    class CustomerManagerTests
+    {
+        public void SaveTest()
+        {
+            // stub logger for only test purposes, I should make it singleton for performance
+            CustomerManager customerManager = new CustomerManager(StubLogger.GetLogger());
+            customerManager.Save();
+        }
+    }
+}
