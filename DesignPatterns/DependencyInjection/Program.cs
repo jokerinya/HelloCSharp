601c3259db5495ff318bd658de5b56dd3cc65be4
Jokerinya <sakacibrahim@gmail.com>
2023-01-02 18:21:57 +0200

    dependency injection pattern has been observed

---
+++ DesignPatterns/DependencyInjection/Program.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Ninject;
+
+namespace DependencyInjection
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // IoC-Inversion of control container implementation
+            IKernel kernel = new StandardKernel();
+            // Singleton will provide performance increase
+            //kernel.Bind<IProductDal>().To<NhProductDal>().InSingletonScope();
+            kernel.Bind<IProductDal>().To<EfProductDal>().InSingletonScope();
+
+            ProductManager productManager = new ProductManager(kernel.Get<IProductDal>());
+
+            productManager.Save();
+
+            Console.ReadLine();
+        }
+    }
+
+    interface IProductDal
+    {
+        void Save();
+    }
+
+    // EntityFramework product responsible class
+    class EfProductDal : IProductDal
+    {
+        public void Save()
+        {
+            Console.WriteLine("Saved with EF");
+        }
+    }
+
+    // Nhibernate product table responsible class
+    class NhProductDal : IProductDal
+    {
+        public void Save()
+        {
+            Console.WriteLine("Saved with NHibernate");
+        }
+    }
+
+    class ProductManager
+    {
+        private IProductDal _productDal;
+
+        public ProductManager(IProductDal productDal)
+        {
+            _productDal = productDal;
+        }
+
+        public void Save()
+        {
+            // Business
+            _productDal.Save();
+        }
+    }
+}
