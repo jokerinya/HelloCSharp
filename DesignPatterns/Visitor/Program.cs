f11977a0f63b1ad86ef82f5369cb4801fd837272
Jokerinya <sakacibrahim@gmail.com>
2023-01-02 15:29:28 +0200

    visitor pattern has been observed

---
+++ DesignPatterns/Visitor/Program.cs
@@ -0,0 +1,120 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Visitor
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Manager johnManager = new Manager { Name = "John", Salary = 1000 };
+            Manager maxManager = new Manager { Name = "Max", Salary = 900 };
+
+            Worker carolwWorker = new Worker { Name = "Carol", Salary = 800 };
+            Worker lizWorker = new Worker { Name = "Liz", Salary = 700 };
+
+            johnManager.Subordinates.Add(maxManager);
+            maxManager.Subordinates.Add(carolwWorker);
+            maxManager.Subordinates.Add(lizWorker);
+
+            OrganizationalStructure organizationalStructure = new OrganizationalStructure(johnManager);
+
+            PayrollVisitor payrollVisitor = new PayrollVisitor();
+            PayriseVisitor payriseVisitor = new PayriseVisitor();
+
+            organizationalStructure.Accept(payrollVisitor);
+            organizationalStructure.Accept(payriseVisitor);
+
+            Console.ReadLine();
+        }
+    }
+
+
+
+    abstract class VisitorBase
+    {
+        public abstract void Visit(Worker worker);
+        public abstract void Visit(Manager manager);
+    }
+
+    class PayrollVisitor : VisitorBase
+    {
+        public override void Visit(Worker worker)
+        {
+            Console.WriteLine($"{worker.Name} paid {worker.Salary}");
+        }
+
+        public override void Visit(Manager manager)
+        {
+            Console.WriteLine($"{manager.Name} paid {manager.Salary}");
+        }
+    }
+
+    class PayriseVisitor : VisitorBase
+    {
+        public override void Visit(Worker worker)
+        {
+            Console.WriteLine($"{worker.Name} salary increased {worker.Salary * (decimal)1.1}");
+        }
+
+        public override void Visit(Manager manager)
+        {
+            Console.WriteLine($"{manager.Name} salary increased {manager.Salary * (decimal)1.1}");
+        }
+    }
+
+    abstract class EmployeeBase
+    {
+        public abstract void Accept(VisitorBase visitor);
+        public string Name { get; set; }
+        public decimal Salary { get; set; }
+    }
+
+    class Manager : EmployeeBase
+    {
+        public List<EmployeeBase> Subordinates { get; set; }
+
+        public Manager()
+        {
+            Subordinates = new List<EmployeeBase>();
+        }
+
+        public override void Accept(VisitorBase visitor)
+        {
+            visitor.Visit(this);
+
+            foreach (var employee in Subordinates)
+            {
+                employee.Accept(visitor);
+            }
+
+        }
+    }
+
+    class Worker : EmployeeBase
+    {
+        public override void Accept(VisitorBase visitor)
+        {
+            visitor.Visit(this);
+        }
+    }
+
+    class OrganizationalStructure
+    {
+        public EmployeeBase Employee;
+
+        public OrganizationalStructure(EmployeeBase firstEmployee)
+        {
+            Employee = firstEmployee;
+        }
+
+        public void Accept(VisitorBase visitor)
+        {
+            Employee.Accept(visitor);
+        }
+    }
+
+}
