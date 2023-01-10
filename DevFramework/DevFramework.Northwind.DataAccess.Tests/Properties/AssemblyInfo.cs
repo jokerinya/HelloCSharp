0c2f7c41eaa987533b0992237e477f92514a29c5
Jokerinya <sakacibrahim@gmail.com>
2023-01-10 18:43:01 +0200

    project structure has been created for pro-backend development

---
+++ DevFramework/DevFramework.Northwind.DataAccess.Tests/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("DevFramework.Northwind.DataAccess.Tests")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("DevFramework.Northwind.DataAccess.Tests")]
+[assembly: AssemblyCopyright("Copyright ©  2023")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("0b28016d-9136-47be-b1b7-976e52c23898")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
