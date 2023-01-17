c43542bda4d75dcdde019e13aa1e47866593e958
Jokerinya <sakacibrahim@gmail.com>
2023-01-17 21:28:25 +0200

    entity framework generic repository and queryable repository has been created

--- DevFramework/DevFramework.Core/DataAccess/IEntityRepository.cs
+++ DevFramework/DevFramework.Core/DataAccess/IEntityRepository.cs
@@ -4,10 +4,7 @@ using System.Linq;
 using System.Linq.Expressions;
 using System.Text;
 using System.Threading.Tasks;
-<<<<<<< HEAD
 using DevFramework.Core.Entities;
-=======
->>>>>>> 16ff61320f35a2f68ff4266a2cb6f8f6696e2211
 
 namespace DevFramework.Core.DataAccess
 {
@@ -16,8 +13,8 @@ namespace DevFramework.Core.DataAccess
         // User can send a filter or it is default null
         List<T> GetList(Expression<Func<T, bool>> filter = null);
         T Get(Expression<Func<T, bool>> filter = null);
-        T Add (T entity);
+        T Add(T entity);
         T Update(T entity);
-        void Delete (T entity);
+        void Delete(T entity);
     }
 }
--- DevFramework/DevFramework.Core/DataAccess/IQueryableRepository.cs
+++ DevFramework/DevFramework.Core/DataAccess/IQueryableRepository.cs
@@ -3,7 +3,6 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-<<<<<<< HEAD
 using DevFramework.Core.Entities;
 
 namespace DevFramework.Core.DataAccess
@@ -11,12 +10,5 @@ namespace DevFramework.Core.DataAccess
     public interface IQueryableRepository<T> where T : class, IEntity, new()
     {
         IQueryable<T> Table { get; }
-=======
-
-namespace DevFramework.Core.DataAccess
-{
-    internal interface IQuaryableRepository
-    {
->>>>>>> 16ff61320f35a2f68ff4266a2cb6f8f6696e2211
     }
 }
