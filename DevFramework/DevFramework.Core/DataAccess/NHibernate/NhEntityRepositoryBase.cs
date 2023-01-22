04e319f1a47f7e551cca596fda22631c2ffc9f52
Jokerinya <sakacibrahim@gmail.com>
2023-01-22 02:18:37 +0200

    nhibernate generic repository and quaryable repository have been created

---
+++ DevFramework/DevFramework.Core/DataAccess/NHibernate/NhEntityRepositoryBase.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using DevFramework.Core.Entities;
+using NHibernate.Linq;
+
+namespace DevFramework.Core.DataAccess.NHibernate
+{
+    internal class NhEntityRepositoryBase<TEntity> : IEntityRepository<TEntity>
+        where TEntity : class, IEntity, new()
+    {
+        private NHibernateHelper _nhibernateHelper;
+
+        public NhEntityRepositoryBase(NHibernateHelper nhibernateHelper)
+        {
+            _nhibernateHelper = nhibernateHelper;
+        }
+
+        public TEntity Add(TEntity entity)
+        {
+            using (var session = _nhibernateHelper.OpenSession())
+            {
+                session.Save(entity);
+                return entity;
+            }
+        }
+
+        public void Delete(TEntity entity)
+        {
+            using (var session = _nhibernateHelper.OpenSession())
+            {
+                session.Delete(entity);
+            }
+        }
+
+        public TEntity Get(System.Linq.Expressions.Expression<Func<TEntity, bool>> filter = null)
+        {
+            using (var session = _nhibernateHelper.OpenSession())
+            {
+                return session.Query<TEntity>().SingleOrDefault(filter);
+            }
+        }
+
+        public List<TEntity> GetList(System.Linq.Expressions.Expression<Func<TEntity, bool>> filter = null)
+        {
+            using (var session = _nhibernateHelper.OpenSession())
+            {
+                return filter == null ?
+                    session.Query<TEntity>().ToList() :
+                    session.Query<TEntity>().Where(filter).ToList();
+            }
+        }
+
+        public TEntity Update(TEntity entity)
+        {
+            using (var session = _nhibernateHelper.OpenSession())
+            {
+                session.Update(entity);
+                return entity;
+            }
+        }
+    }
+}
---
+++ DevFramework/DevFramework.Core/DataAccess/NHibernate/NhQueryableRepository.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using DevFramework.Core.Entities;
+
+namespace DevFramework.Core.DataAccess.NHibernate
+{
+    internal class NhQueryableRepository<T> : IQueryableRepository<T>
+        where T : class, IEntity, new()
+    {
+        private NHibernateHelper _nHibernateHelper;
+        private IQueryable<T> _entities;
+
+        public NhQueryableRepository(NHibernateHelper nHibernateHelper)
+        {
+            _nHibernateHelper = nHibernateHelper;
+        }
+
+        public IQueryable<T> Table
+        {
+            get { return this.Entites; }
+        }
+
+        public virtual IQueryable<T> Entites
+        {
+            get { return _entities ?? (_entities = _nHibernateHelper.OpenSession().Query<T>()); }
+        }
+    }
+}
